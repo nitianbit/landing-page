c0c1365a3d64814827e5448c36a2d7eb121ecd01
Tushar Mangla <tushar.mangla1120@gmail.com>
2024-07-14 17:03:45 +0530

    remove admin layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Im
 
 import './App.css';
 import LandingPage from './components/LandingPage/LandingPage.jsx';
-import AdminLayout from './components/Admin/AdminLayout.jsx';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,7 +11,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<AdminLayout />} />
       </Routes>
     </Router>
   );
