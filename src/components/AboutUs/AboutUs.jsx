5b5520ce0b7d3bcca0ff55128ebbd0ea951e0753
tushar-mangla <82701366+tushar-mangla@users.noreply.github.com>
2024-09-08 16:14:08 +0530

    Update AboutUs.jsx

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,1 +1,28 @@
 
+import React from 'react'
+import AboutUsCard from './AboutUsCard'
+
+const AboutUs = () => {
+  return (
+    <div className='p-6'>
+      <div className='flex justify-center'>WHAT WE OFFER</div>
+      <div className='text-3xl flex justify-center font-bold pb-8'>Featured Properties</div>
+
+      <div class="grid gap-4 mx-8">
+        <div class="flex justify-center gap-3">
+          <AboutUsCard text="Residential Apartments"/>
+          <AboutUsCard text="Residential Flats"/>
+          <AboutUsCard text="Shop-Cum Offices"/>
+          {/* <AboutUsCard text="Shop-Cum Offices"/> */}
+        </div>
+        <div class="flex justify-center gap-4">
+          <AboutUsCard text="Shop-Cum Offices"/>
+          <AboutUsCard text="Residential Plots"/>
+          <AboutUsCard text="Residential Vilas"/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AboutUs
