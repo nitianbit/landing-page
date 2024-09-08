3a67187478467c6ca518562259bfb495e7b262ba
tushar-mangla <82701366+tushar-mangla@users.noreply.github.com>
2024-09-08 16:14:41 +0530

    Update AboutUsCard.jsx

--- src/components/AboutUs/AboutUsCard.jsx
+++ src/components/AboutUs/AboutUsCard.jsx
@@ -1,1 +1,16 @@
 
+import React from 'react'
+import apartment from '../../assets/images/delhi.jpg'
+
+const AboutUsCard = ({text}) => {
+  return (
+    <div class="flex-1  border p-4">
+         <img  style={{
+            aspectRatio:1
+        }} src={apartment}/>
+        <div className='flex justify-center font-medium mt-2'>{text}</div>
+    </div>
+  )
+}
+
+export default AboutUsCard
