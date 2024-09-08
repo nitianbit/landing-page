6ffea51899ca0a3f25cb2bc78938acd2c3049a4a
Nitian Bit <155094550+nitianbit@users.noreply.github.com>
2024-09-08 23:10:39 +0530

    Update ServiceCard.jsx

--- src/components/OurServices/ServiceCard.jsx
+++ src/components/OurServices/ServiceCard.jsx
@@ -1,1 +1,15 @@
+import React from 'react'
+import apartment from '../../assets/images/delhi.jpg'
 
+const ServiceCard = ({text}) => {
+  return (
+    <div className="flex-1  border p-4">
+         <img  style={{
+            aspectRatio:1
+        }} src={apartment}/>
+        <div className='flex justify-center font-medium mt-2'>{text}</div>
+    </div>
+  )
+}
+
+export default ServiceCard
