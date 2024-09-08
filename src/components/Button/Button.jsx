301df706fce3a2d7f5c31fcc25c50940a2689129
tushar-mangla <82701366+tushar-mangla@users.noreply.github.com>
2024-09-08 16:15:52 +0530

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,1 +1,12 @@
 
+import React from 'react'
+
+const Button = ({ text,href, onClick, className }) => {
+  return (
+    <button  onClick={onClick} className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded-full ${className}`}>
+      {text}
+    </button>
+  )
+}
+
+export default Button
