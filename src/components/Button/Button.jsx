dd5e01f3858094e7c2d1d3f4fffc60d9da5e4726
Nitian Bit <155094550+nitianbit@users.noreply.github.com>
2024-09-08 23:08:17 +0530

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,10 @@
-
 import React from 'react'
 
-const Button = ({ text,href, onClick, className }) => {
+const Button = ({ text,href, onClick, className, style }) => {
   return (
-    <button  onClick={onClick} className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded-full ${className}`}>
+    <button style={{
+      backgroundColor:"#64506b",
+      ...style}}  onClick={onClick} className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded-full ${className}`}>
       {text}
     </button>
   )
