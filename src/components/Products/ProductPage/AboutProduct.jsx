096cb0f21ef9dd009ab440091ed4bfd6346e4eef
tushar-mangla <82701366+tushar-mangla@users.noreply.github.com>
2024-09-08 16:24:45 +0530

    Update AboutProduct.jsx

--- src/components/Products/ProductPage/AboutProduct.jsx
+++ src/components/Products/ProductPage/AboutProduct.jsx
@@ -1,1 +1,19 @@
+import React from 'react'
+import Button from '../../Button/Button'
 
+const AboutProduct = ({productName}) => {
+  return (
+    <div className="p-6 border-b">
+    <div className='text-3xl flex justify-center font-bold pb-8'>{productName}</div>
+    <div>
+        <Button text="RERA Approved" className="px-4"/>
+        <div className='mt-2'>What is RERA?</div>
+        <div>The Real Estate (Regulation and Development) Act, 2016 is an Act of the Parliament of India which seeks to protect home-buyers as well as help boost investments in the real estate industry</div>
+        <div className='mt-4'>RERA ID:</div>
+        <div># P50500000005</div>
+    </div>
+</div>
+  )
+}
+
+export default AboutProduct
