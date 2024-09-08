35ad695d141cb2c15acc2517f51b1a22071e74ef
Nitian Bit <155094550+nitianbit@users.noreply.github.com>
2024-09-09 00:31:18 +0530

    add type Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const Button = ({ text,href, onClick, className, style }) => {
+const Button = ({ text,href, onClick, className, style, type="" }) => {
   return (
-    <button style={{
+    <button
+     type={type}
+    style={{
       backgroundColor:"#64506b",
       ...style}}  onClick={onClick} className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded-full ${className}`}>
       {text}
