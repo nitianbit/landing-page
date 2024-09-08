056bda0afb32f07b06e43de28630844e56c2ddc9
tushar-mangla <82701366+tushar-mangla@users.noreply.github.com>
2024-09-08 16:20:47 +0530

    Update Form1Section.jsx

--- src/components/Forms/Form1Section.jsx
+++ src/components/Forms/Form1Section.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react'
 import Form1 from './Form1'
 
-const Form1Section = ({ form, handleSubmit }) => {
+const Form1Section = ({ form, handleSubmit, isFormNeeded=false }) => {
     return (
-        <div className="flex flex-wrap " >
-            <div className="w-full md:w-7/10 border-b border-black" style={{ maxHeight: "85vh" }}>
+        <div className='flex flex-wrap ' >
+            <div className={`w-full md:w-${isFormNeeded ? "7" : "10"}/10 border-b border-black`} style={{ maxHeight: "85vh" }}>
                 <img
                     src="https://i.ibb.co/mvdqV88/Screenshot-2024-06-25-at-11-33-04-AM.png"
                     alt="Large description"
@@ -14,10 +14,10 @@ const Form1Section = ({ form, handleSubmit }) => {
             </div>
 
             {/* Form Section */}
-            <div className="w-full md:w-3/10 bg-[#f6f2ec] p-8 overflow-auto border-b border-black" style={{ maxHeight: "85vh" }}>
+           {isFormNeeded && <div className="w-full md:w-3/10 bg-[#f6f2ec] p-8 overflow-auto border-b border-black" style={{ maxHeight: "85vh" }}>
                 <h2 className="text-2xl mb-6 p-4">Let's Find Your Dream Properties, Request a Callback</h2>
                 <Form1 form={form} handleSubmit={handleSubmit} />
-            </div>
+            </div>}
         </div>
     )
 }
