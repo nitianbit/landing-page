998414936cb1b4fd5d6231973ecc313111132cf1
tushar-mangla <82701366+tushar-mangla@users.noreply.github.com>
2024-09-08 16:17:02 +0530

    Update CitiesCard.jsx

--- src/components/Cities/CitiesCard.jsx
+++ src/components/Cities/CitiesCard.jsx
@@ -1,1 +1,15 @@
+import React from 'react'
+import delhi from '../../assets/images/delhi.jpg'
 
+const CitiesCard = ({cityName}) => {
+  return (
+    <div className='flex-1 w-1/5 border p-4 flex flex-col justify-center'>
+        <img  style={{
+            aspectRatio:1
+        }} src={delhi}/>
+        <div className='flex justify-center font-medium mt-2'>{cityName}</div>
+    </div>
+  )
+}
+
+export default CitiesCard
