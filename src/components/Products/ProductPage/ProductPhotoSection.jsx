0c54417366b44c25b9a3729077d3d26f926fe9b6
tushar-mangla <82701366+tushar-mangla@users.noreply.github.com>
2024-09-08 16:26:05 +0530

    Update ProductPhotoSection.jsx

--- src/components/Products/ProductPage/ProductPhotoSection.jsx
+++ src/components/Products/ProductPage/ProductPhotoSection.jsx
@@ -1,1 +1,22 @@
+import React from 'react'
 
+const ProductPhotoSection = () => {
+    return (
+        <div className="border-t border-b p-6">
+            <div className='text-3xl flex justify-center font-bold pb-8'>More Photos</div>
+            <div class="grid gap-4 grid-flow-col overflow-x-auto horizontal-scroll">
+                <img src="image1.jpg" class="h-48 w-64 object-cover" alt="Image 1" />
+                <img src="image2.jpg" class="h-48 w-64 object-cover" alt="Image 2" />
+                <img src="image3.jpg" class="h-48 w-64 object-cover" alt="Image 3" />
+                <img src="image3.jpg" class="h-48 w-64 object-cover" alt="Image 3" />
+                <img src="image3.jpg" class="h-48 w-64 object-cover" alt="Image 3" />
+                <img src="image3.jpg" class="h-48 w-64 object-cover" alt="Image 3" />
+                <img src="image3.jpg" class="h-48 w-64 object-cover" alt="Image 3" />
+                <img src="image3.jpg" class="h-48 w-64 object-cover" alt="Image 3" />
+                <img src="image3.jpg" class="h-48 w-64 object-cover" alt="Image 3" />
+            </div>
+        </div>
+    )
+}
+
+export default ProductPhotoSection
