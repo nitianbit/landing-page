f4cc25a5a224b252badb5a1b9b9577f865e27b7f
Nitian Bit <155094550+nitianbit@users.noreply.github.com>
2024-09-10 00:58:17 +0530

    Update Clients.jsx

--- src/components/Clients/Clients.jsx
+++ src/components/Clients/Clients.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Form1Section from '../Forms/Form1Section'
+import { useUserContext } from '../../context/UserContext';
+import {CLIENTENDPOINTS} from './constant'
+import { doGET } from '../../utils/HttpUtils';
 
 const Client = () => {
+  const { project, setProject } = useUserContext()
+  const [form, setForm] = useState(null)
+  const getForm = async (projectId) => {
+    try {
+      const query = `type=client&projectId=${projectId}`;
+      const response = await doGET(CLIENTENDPOINTS.getForm(query));
+      // const formsArray = Array.from({ length: 3 }, (_, index) => response.find(form => form.formIndex === index) || []);
+
+      setForm(response);
+    } catch (error) {
+      console.error("Error fetching forms:", error);
+    }
+  };
+
+  useEffect(() => {
+    if(project){
+       getForm(project?._id)
+    }
+  }, [project])
+
   return (
     <div>
-        <Form1Section isFormNeeded={true}/>
+        <Form1Section  form={form} isFormNeeded={true}/>
     </div>
   )
 }
