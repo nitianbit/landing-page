bd674b05d54b126ae3e8dd4a58b0e4543e8e1709
Nitian Bit <155094550+nitianbit@users.noreply.github.com>
2024-09-10 00:50:32 +0530

    Update Cities.jsx

--- src/components/Cities/Cities.jsx
+++ src/components/Cities/Cities.jsx
@@ -1,46 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CitiesCard from './CitiesCard'
+import apartment from '../../assets/images/delhi.jpg'
+import "./styles.css"
 
-const Cities = () => {
-    return (
-        <div className="border-t border-b p-6">
-            <div className='text-3xl flex justify-center font-bold pb-8'>Popular Cities</div>
-            <div class="grid gap-4 grid-flow-col overflow-x-auto horizontal-scroll">
-                <div>
-                    <div className='flex justify-center gap-4'>
-                        <CitiesCard cityName="Delhi" />
-                        <CitiesCard cityName="Gurgaon" />
-                        <CitiesCard cityName="Faridabad" />
-                        <CitiesCard cityName="Noida" />
-                        <CitiesCard cityName="Hyderabad" />
-                    </div>
-                    <div className='flex justify-center gap-4'>
-                        <CitiesCard cityName="Delhi" />
-                        <CitiesCard cityName="Gurgaon" />
-                        <CitiesCard cityName="Faridabad" />
-                        <CitiesCard cityName="Noida" />
-                        <CitiesCard cityName="Hyderabad" />
-                    </div>
-                </div>
+const Service = () => {
+  const [data, setData] = useState([
+    { title: 'Residential Apartments', src: apartment },
+    { title: 'Residential Flats', src: apartment },
+    { title: 'Residential Builder Floors', src: apartment },
+    { title: 'Residential Duplex', src: apartment },
+    { title: 'Residential Vilas', src: apartment },
+    { title: 'Residential Plots', src: apartment },
+    { title: 'Residential Offices', src: apartment },
+    { title: 'Residential Shops', src: apartment },
+
+    { title: 'Residential Apartments', src: apartment },
+    { title: 'Residential Flats', src: apartment },
+    { title: 'Residential Builder Floors', src: apartment },
+    { title: 'Residential Duplex', src: apartment },
+    { title: 'Residential Vilas', src: apartment },
+    { title: 'Residential Plots', src: apartment },
+    { title: 'Residential Offices', src: apartment },
+    { title: 'Residential Shops', src: apartment }, 
+  ]);
+
+  return (
+    <div className='p-6'>
+    <div className='flex justify-center'>WHAT WE OFFER</div>
+    <div className='text-3xl flex justify-center font-bold pb-8'>Featured Properties</div>
+    
+    <div className="w-full overflow-x-auto scrollbar-hide">
+        <div className="flex space-x-4">
+          <div className='flex flex-col'>
+
+            <div className="flex flex-row space-x-4 gap-4 mb-5">
+              {data.map((item, index) => (
+                index % 2 === 0 && (
+                  <CitiesCard text={item.title} key={index} />
+                )
+              ))}
             </div>
-            {/* <div class="grid gap-4">
-                <div class="flex justify-center gap-4">
-                    <CitiesCard cityName="Delhi" />
-                    <CitiesCard cityName="Gurgaon" />
-                    <CitiesCard cityName="Faridabad" />
-                    <CitiesCard cityName="Noida" />
-                    <CitiesCard cityName="Hyderabad" />
-                </div>
-                <div class="flex justify-center gap-4">
-                    <CitiesCard cityName="Mumbai" />
-                    <CitiesCard cityName="Pune" />
-                    <CitiesCard cityName="Bengaluru" />
-                    <CitiesCard cityName="Chennai" />
-                    <CitiesCard cityName="Kolkata" />
-                </div>
-            </div> */}
+            <div className="flex flex-row space-x-4 gap-4 mb-5">
+              {data.map((item, index) => (
+                index % 2 !== 0 && (
+                  <CitiesCard text={item.title} key={index} />
+                )
+              ))}
+            </div>
+          </div>
         </div>
-    )
+      </div>
+
+       
+ 
+     
+
+  </div>
+  )
 }
 
-export default Cities
+export default Service
