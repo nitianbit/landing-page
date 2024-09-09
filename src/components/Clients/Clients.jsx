8ea4a421f3d2c8a02b2dfaa6b803f4d709151b17
Nitian Bit <155094550+nitianbit@users.noreply.github.com>
2024-09-09 17:11:20 +0530

    Update Clients.jsx

--- src/components/Clients/Clients.jsx
+++ src/components/Clients/Clients.jsx
@@ -1,1 +1,12 @@
+import React from 'react'
+import Form1Section from '../Forms/Form1Section'
 
+const Client = () => {
+  return (
+    <div>
+        <Form1Section isFormNeeded={true}/>
+    </div>
+  )
+}
+
+export default Client
