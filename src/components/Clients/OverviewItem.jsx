35a91fdcc9e522367c4bf2fff8a2d2e8a8331af8
Nitian Bit <155094550+nitianbit@users.noreply.github.com>
2024-09-12 01:57:31 +0530

    Update OverviewItem.jsx

--- src/components/Clients/OverviewItem.jsx
+++ src/components/Clients/OverviewItem.jsx
@@ -1,1 +1,13 @@
+import React from 'react'
 
+const OverviewItem = ({icon, title, description}) => {
+    return (
+        <div className="text-center">
+            <img src={icon} alt={`${title} Icon`} className="mx-auto mb-4" />
+            <p className="font-semibold my-4">{title}</p>
+            <div className="text-md text-gray-600">{description}</div>
+        </div>
+    )
+}
+
+export default OverviewItem
