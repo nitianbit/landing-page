de533740aa45d53cf68ae52374aebd83458ff774
Nitian Bit <155094550+nitianbit@users.noreply.github.com>
2024-09-12 01:56:55 +0530

    Update data.jsx

--- src/components/Clients/data.jsx
+++ src/components/Clients/data.jsx
@@ -1,1 +1,109 @@
-
+export const clientData = {
+    _id: 1,
+    title: 'Universal Square',
+    developer: 'Auric Group',
+    location: 'Sector 79, Faridaad',
+    description: 'Independent SCO Floors and Commercial SCO Plots',
+    startingPrice: 'Starting ₹49 Lakh*',
+    overviewItems: [
+        {
+            image: '',
+            title: 'Targeted Audience',
+            description: ' We use advanced data analysis to identify and reach out to buyers who are actively looking for properties, leads to sales maximisation.'
+        },
+        {
+            image: '',
+            title: 'Better Conversion Rates',
+            description: 'Your sales team can connect with buyers and focus on closing deals, boosting your conversion rates, and shortening your sales cycles.'
+        },
+        {
+            image: '',
+            title: ' Time and Cost Efficiency',
+            description: ' We filter out low-quality inquiries, allowing you to save valuable time and resources while focusing on highpotential prospects.'
+        },
+        {
+            image: '',
+            title: ' Custom Solutions',
+            description: " Whether you're selling residential, commercial, or luxury properties, our solutions are tailored to your unique needs."
+        },
+    ],
+    photos: [
+        "https://www.w3schools.com/html/images/13_html_images.png",
+        "https://www.w3schools.com/html/images/13_html_images.png",
+        "https://www.w3schools.com/html/images/13_html_images.png",
+        "https://www.w3schools.com/html/images/13_html_images.png",
+        "https://www.w3schools.com/html/images/13_html_images.png",
+        "https://www.w3schools.com/html/images/13_html_images.png",
+        "https://www.w3schools.com/html/images/13_html_images.png",
+        "https://www.w3schools.com/html/images/13_html_images.png",
+        "https://www.w3schools.com/html/images/13_html_images.png"
+    ],
+    amenities: {
+        type: 'list',
+        data: [
+            { name: "Restaurants", image: "https://www.w3schools.com/html/images/13_html_images.png" },
+            { name: "Food Court", image: "https://www.w3schools.com/html/images/13_html_images.png" },
+            { name: "Multiplex", image: "https://www.w3schools.com/html/images/13_html_images.png" },
+            { name: "Shopping Area", image: "https://www.w3schools.com/html/images/13_html_images.png" },
+            { name: "Hypermarket", image: "https://www.w3schools.com/html/images/13_html_images.png" },
+            { name: "Anchor Store", image: "https://www.w3schools.com/html/images/13_html_images.png" },
+            { name: "Banquet", image: "https://www.w3schools.com/html/images/13_html_images.png" },
+            { name: "Pubs & Discos", image: "https://www.w3schools.com/html/images/13_html_images.png" },
+            { name: "Gaming Zone", image: "https://www.w3schools.com/html/images/13_html_images.png" },
+            { name: "Office Spaces", image: "https://www.w3schools.com/html/images/13_html_images.png" },
+            { name: "Retail", image: "https://www.w3schools.com/html/images/13_html_images.png" },
+            { name: "Open Air Food Court", image: "https://www.w3schools.com/html/images/13_html_images.png" },
+        ]
+    },
+    locationAdvantages: {
+        type: 'list',
+        data: [
+            { time: "00", description: "MINUTES ADJACENT TO THE INSTITUTIONAL BELT" },
+            { time: "05", description: "MINUTES DRIVE TO THE NEAREST METRO STATION" },
+            { time: "10", description: "MINUTES DRIVE TO FORTIS ESCORT HOSPITAL" },
+            { time: "15", description: "MINUTES DRIVE TO GREATER NOIDA" },
+            { time: "15", description: "MINUTES DRIVE TO BADARPUR FLY OVER" },
+            { time: "20", description: "MINUTES DRIVE TO SOUTH DELHI" },
+            { time: "35", description: "MINUTES DRIVE TO GURGAON" },
+            { time: "35", description: "MINUTES DRIVE TO AIRPORT" },
+        ]
+    },
+    highlights: [
+        "Welcome Court",
+        "The Front Square",
+        "The Central Square",
+        "The Rear Square",
+        "The Northern Square (Walk | Drive | Park)",
+        "The Southend Square (Walk | Drive | Park)",
+        "The Westend Square (Walk | Drive | Park)",
+        "The Basement Is The Very Base Of Business",
+        "The Universal Link",
+        "Building Height Multi-level Store Height",
+        "Ample surface car parking",
+        "Wider service alleys",
+        "Facilitate by wide range plazas",
+        "Distinctly designed space for pedestrian and vehicular movement"
+    ],
+    about: {
+        type: 'html',
+        value: `
+            <h1>Independent Commercial Space</h1>
+            <h2>A Vision of Future, Success, Change, and Growth</h2>
+            <p>Universal Square is a project that incubates hope, business spirit, and countless opportunities for growth. Designed with a deep understanding of retail and business, inspired by the world’s best commercial projects. Universal Square includes an internal environment that facilitates futuristic, strategic, and long-term visions.</p>
+            <p>Situated in Faridabad Sector 79, the Universal Square is planned to offer a modern lifestyle to all the residents. It covers an area of 13.87 Acre giving enough green space to residents. This project encompasses over 227 units.</p>
+            <p>The units are all under construction. Officially, this grand project's launch date is 01 April 2023. Its official date of possession is 01 June 2027.</p>
+            <p>The commencement certificate of the impressive Universal Square project has been granted. Known for their quality constructions, Auric Group has developed the Universal Square project. The address of this smartly planned project is Sector 79, Neharpar, Faridabad - 121002, Haryana. At Universal Square, you can enjoy a modern lifestyle as all conveniences are easily available.</p>
+        `
+    },
+    aboutDeveloper: {
+        type: 'html',
+        value: `
+            <h1>Independent Commercial Space</h1>
+            <h2>A Vision of Future, Success, Change, and Growth</h2>
+            <p>Universal Square is a project that incubates hope, business spirit, and countless opportunities for growth. Designed with a deep understanding of retail and business, inspired by the world’s best commercial projects. Universal Square includes an internal environment that facilitates futuristic, strategic, and long-term visions.</p>
+            <p>Situated in Faridabad Sector 79, the Universal Square is planned to offer a modern lifestyle to all the residents. It covers an area of 13.87 Acre giving enough green space to residents. This project encompasses over 227 units.</p>
+            <p>The units are all under construction. Officially, this grand project's launch date is 01 April 2023. Its official date of possession is 01 June 2027.</p>
+            <p>The commencement certificate of the impressive Universal Square project has been granted. Known for their quality constructions, Auric Group has developed the Universal Square project. The address of this smartly planned project is Sector 79, Neharpar, Faridabad - 121002, Haryana. At Universal Square, you can enjoy a modern lifestyle as all conveniences are easily available.</p>
+        `
+    },
+}
