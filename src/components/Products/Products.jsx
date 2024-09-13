6d61ac36b757bb24b2b166063748812bcaf45862
Nitian Bit <155094550+nitianbit@users.noreply.github.com>
2024-09-14 02:17:40 +0530

    Update Products.jsx

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,45 @@ import { Card } from 'flowbite-react'
 import React, { useEffect, useState } from 'react'
 import { doGET } from '../../utils/HttpUtils';
 import { useNavigate } from 'react-router-dom';
+import universalSquare from '../../assets/home/hot-properties/Universal Square.png'
+import vistaCorner from '../../assets/home/hot-properties/Vista Corner.png'
+import ekamProsperityHomes from '../../assets/home/hot-properties/EKAM by Prosperity Homes.png'
+import manshaAffordable from '../../assets/home/hot-properties/MANSHA Affordable.png'
+import ProductCard from './ProductCard';
+
+const data = [{
+    heading1: "Universal Square By Auric",
+    heading2: "Sector 79, Faridabad",
+    heading3: "Commercial Offices/Shops",
+    heading4: "Starting @45 Lakh*",
+    src: universalSquare,
+    onClick:(()=>{navigate(`/product/${products[0]?._id}`)})
+},
+{
+    heading1: "Vista Corner",
+    heading2: "Sector 89, Gurgaon",
+    heading3: "Commercial Offices/Shops",
+    heading4: "Starting @75 Lakh*",
+    src: vistaCorner,
+    onClick:(()=>{navigate(`/product/${products[1]?._id}`)})
+},
+{
+    heading1: "EKAM by Prosperity Homes",
+    heading2: "Velimela, Hyderabad",
+    heading3: "Residential Apartments",
+    heading4: "Starting @1.16 Crore*",
+    src: ekamProsperityHomes,
+    onClick:(()=>{})
+},
+{
+    heading1: "MASNSHA Affordable",
+    heading2: "Sector 104, Faridabad",
+    heading3: "Residential Flats",
+    heading4: "Starting @31.25 Lakh*",
+    src: manshaAffordable,
+    onClick:(()=>{})
+}]
+
 
 const Products = ({ project }) => {
     const navigate = useNavigate();
@@ -21,57 +60,9 @@ const Products = ({ project }) => {
     }, [project?._id])
     return (
         <>
-            <Card
-                className="max-w-sm shadow-none border-none cursor-pointer"
-                imgAlt="Meaningful alt text for an image that is not purely decorative"
-                imgSrc="https://i.ibb.co/10YBYbg/Screenshot-2024-06-27-at-10-38-03-AM.png"
-                onClick={() => navigate(`/product/${products[0]?._id}`)}
-            >
-                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
-                    Universal Square By Auric
-                </h5>
-                <p className="font-normal text-gray-700 dark:text-gray-400 text-center">
-                    Independent SCO FLOORS Sector 79, Faridabad
-                </p>
-                <h3 className="text-xl tracking-tight text-gray-900 dark:text-white text-center">
-                    Starting @45 Lakh*
-                </h3>
-            </Card>
-            <Card
-                className="max-w-sm shadow-none border-none cursor-pointer"
-                imgAlt="Meaningful alt text for an image that is not purely decorative"
-                imgSrc="https://i.ibb.co/CKm1hMd/Screenshot-2024-06-27-at-10-38-19-AM.png"
-                onClick={() => navigate(`/product/${products[1]?._id}`)}
-            >
-                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
-                    MANSHA ORCHID PLOTS
-                </h5>
-                <p className="font-normal text-gray-700 dark:text-gray-400 text-center">
-                    Gated Plot Township Sector 110, Faridabad
-
-                </p>
-                <h3 className="text-xl tracking-tight text-gray-900 dark:text-white text-center">
-                    Starting @45000/sq. yard.*
-                </h3>
-            </Card>
-            <Card
-                className="max-w-sm shadow-none border-none cursor-pointer"
-                imgAlt="Meaningful alt text for an image that is not purely decorative"
-                imgSrc="https://i.ibb.co/DgKfqC9/Screenshot-2024-06-27-at-10-38-30-AM.png"
-                onClick={() => navigate(`/product/${products[2]?._id}`)}
-            >
-                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
-
-                    MANSHA AFFORDABLE FLATS
-                </h5>
-                <p className="font-normal text-gray-700 dark:text-gray-400 text-center">
-
-                    2BHK & 4BHK Flats Sector 104, Faridabad
-                </p>
-                <h3 className="text-xl tracking-tight text-gray-900 dark:text-white text-center">
-                    Starting @31.25 Lakh*
-                </h3>
-            </Card>
+         {data?.map((item, index)=>{
+            return <ProductCard heading1={item?.heading1} heading2={item?.heading2} heading3={item?.heading3} heading4={item?.heading4} src={item?.src} onClick={item?.onClick}/>
+        })}
         </>
     )
 }
