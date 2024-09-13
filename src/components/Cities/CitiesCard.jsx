8751c22748ed8ee85b917c46923e4d4a8551cc4e
Nitian Bit <155094550+nitianbit@users.noreply.github.com>
2024-09-14 01:43:09 +0530

    Update CitiesCard.jsx

--- src/components/Cities/CitiesCard.jsx
+++ src/components/Cities/CitiesCard.jsx
@@ -1,14 +1,13 @@
 
 import React from 'react'
-import apartment from '../../assets/images/delhi.jpg'
 import "./styles.css"
 
-const CitiesCard = ({text}) => {
+const CitiesCard = ({text, src}) => {
   return (
     <div className="flex-1 p-4 service-image">
          <img  style={{
             aspectRatio:1
-        }} src={apartment}/>
+        }} src={src}/>
         <div className='flex justify-center font-medium mt-2'>{text}</div>
     </div>
   )
