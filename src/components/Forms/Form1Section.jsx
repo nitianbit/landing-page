5466f2f17dae18084dd604e0ff10499bdd36ca57
Nitian Bit <155094550+nitianbit@users.noreply.github.com>
2024-09-14 03:06:45 +0530

    Update Form1Section.jsx

--- src/components/Forms/Form1Section.jsx
+++ src/components/Forms/Form1Section.jsx
@@ -1,19 +1,17 @@
 import React, { useEffect } from 'react'
 import Form1 from './Form1'
 
-const Form1Section = ({ form, isFormNeeded=false }) => {
+const Form1Section = ({ form, isFormNeeded=false, src }) => {
     return (
         <div className='flex flex-wrap ' >
             <div className={`w-full md:w-${isFormNeeded ? "7" : "10"}/10 border-b border-black`} style={{ maxHeight: "85vh" }}>
                 <img
-                    src="https://i.ibb.co/mvdqV88/Screenshot-2024-06-25-at-11-33-04-AM.png"
+                    src={src}
                     alt="Large description"
                     className="w-full h-full object-cover"
                     style={{ maxHeight: 'calc(100vh - 4rem)' }} // Adjust for navbar height if needed
                 />
             </div>
-
-            {/* Form Section */}
            {isFormNeeded && <div className="w-full md:w-3/10 bg-[#f6f2ec] p-8 overflow-auto border-b border-black" style={{ maxHeight: "85vh" }}>
                 <h3 className="text-xl mb-6 p-4">GENERATE HIGHT QUALITY LEADS FOR REAL ESTATE</h3>
                 <Form1 form={form}  />
