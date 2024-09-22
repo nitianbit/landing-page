c9ee3dd95776ace8136104aefe88c7727e51d082
Nitian Bit <155094550+nitianbit@users.noreply.github.com>
2024-09-23 01:52:52 +0530

    Delete src directory

--- src/utils/helper.js
+++
@@ -1,8 +0,0 @@
-export const getUTMParameters = () => {
-    const params = new URLSearchParams(window.location.search);
-    const queryParameters = {};
-    params.forEach((value, key) => {
-        queryParameters[key] = value;
-    });
-    return queryParameters;
-};
