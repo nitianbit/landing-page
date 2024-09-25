bfa6c05888bd28e311b458523fbb9365eb2418b8
Nitian Bit <155094550+nitianbit@users.noreply.github.com>
2024-09-26 03:37:54 +0530

    Update footer.js

--- src/screens/footer.js
+++ src/screens/footer.js
@@ -84,7 +84,7 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className="footerbottom">
-                    <p className="text-start text-white">
+                    <p className="text-center text-white">
                         Copyright @apkiproperty.com. All Rights Reserved.
                     </p>
                 </div>
