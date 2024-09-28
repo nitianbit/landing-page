f19fcfc3eae789fc875d8ae04684f312c15cdff2
Nitian Bit <155094550+nitianbit@users.noreply.github.com>
2024-09-28 17:23:06 +0530

    Update helper.js

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -6,3 +6,26 @@ export const getUTMParameters = () => {
     });
     return queryParameters;
 };
+
+export const validateIndianMobileNumber = (mobileNumber)=> {
+    const pattern = /^[6-9]\d{9}$/;
+    return pattern.test(mobileNumber);
+}
+
+export const validateEmail = (email)=> {
+    const pattern = /^[\w.-]+@[\w.-]+\.\w+$/;
+    return pattern.test(email);
+}
+
+export const validateFields = (fields, data, setErrors) => {
+    const errors = {};
+    fields.forEach(field => {
+        if (field.type === 'email' && !validateEmail(data[field.name])) {
+            errors[field.name] = 'Invalid email address';
+        }
+        if (field.name === 'mobile' && !validateIndianMobileNumber(data[field.name])) {
+            errors[field.name] = 'Invalid mobile number';
+        }
+    });
+    return errors;
+};
