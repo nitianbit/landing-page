f1c2c9161d005b460c753f4669c84d0c0e23e580
Nitian Bit <155094550+nitianbit@users.noreply.github.com>
2024-09-30 14:55:23 +0530

    Update ThankYouPage.js

--- src/components/ThankYou/ThankYouPage.js
+++ src/components/ThankYou/ThankYouPage.js
@@ -1,18 +1,46 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const ThankYouPage = ({isOpen, setIsOpen}) => {
+const ThankYouPage = ({ isOpen, setIsOpen }) => {
   // const [isOpen, setIsOpen] = useState(false)
+  const navigate = useNavigate()
   return (
-    <div>
-    <div class="inqure">
+    <div style={{
+      fontStyle:"Garet, sans-serif"
+    }}>
+      <div class="inqure" style={{
+        color:"#64506b",
+        padding:"169px 0px"
+      }}>
         {/* <span class="icon-close2 popcls" onClick={() => { setIsOpen(!isOpen) }}></span> */}
-        <h3 className='text-center'>Thank You!</h3>
-        <p className='text-center'>
-            We have successfully registered your details.<br></br>
-            We will get in touch with you shortly.
+        <h3  className='text-center' style={{
+          fontSize:"30px"
+        }}>Thank You!</h3>
+        <p style={{
+          marginTop:"30px",
+          marginBottom:"30px",
+          fontSize:"16px",
+          fontWeight:600
+        }} className='text-center'>
+          We have successfully registered your details.<br></br>
+          We will get in touch with you shortly.
         </p>
+        <div style={{
+          display:"flex",
+          justifyContent:"center"
+        }}>
+          <input type="submit" onClick={() => {
+            navigate("/")
+          }} name="" value="FOR MORE INFORMATION, KINDLY VISIT HERE"  className='button text-center'
+          
+          style={{
+            fontStyle:"'Garet', sans-serif;",
+            fontSize:"13px"
+          }}
+          ></input>
+        </div>
+      </div>
     </div>
-</div>
   )
 }
 
