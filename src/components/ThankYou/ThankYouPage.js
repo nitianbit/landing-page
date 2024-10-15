bfb639eda0193b33e472b8c9a468b9ce2e90fc67
Nitian Bit <155094550+nitianbit@users.noreply.github.com>
2024-10-15 23:08:34 +0530

    Update ThankYouPage.js

--- src/components/ThankYou/ThankYouPage.js
+++ src/components/ThankYou/ThankYouPage.js
@@ -1,46 +1,52 @@
 import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import downloadPdf from '../DownloadPdf'
+import ReactPixel from 'react-facebook-pixel'
 
 const ThankYouPage = ({ isOpen, setIsOpen }) => {
   // const [isOpen, setIsOpen] = useState(false)
   const navigate = useNavigate()
-  useEffect(()=>{
-     window.scrollTo(0, 0);
-  },[])
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [])
+
+  useEffect(() => {
+    ReactPixel.init('1072641991236564')
+    ReactPixel.pageView()
+  }, [])
+
   return (
     <div style={{
-      fontStyle:"Garet, sans-serif"
+      fontStyle: "Garet, sans-serif"
     }}>
       <div class="inqure" style={{
-        color:"#64506b",
-        padding:"169px 0px"
+        color: "#64506b",
+        padding: "169px 0px"
       }}>
         {/* <span class="icon-close2 popcls" onClick={() => { setIsOpen(!isOpen) }}></span> */}
-        <h3  className='text-center' style={{
-          fontSize:"30px"
+        <h3 className='text-center' style={{
+          fontSize: "30px"
         }}>Thank You!</h3>
         <p style={{
-          marginTop:"30px",
-          marginBottom:"30px",
-          fontSize:"16px",
-          fontWeight:600
+          marginTop: "30px",
+          marginBottom: "30px",
+          fontSize: "16px",
+          fontWeight: 600
         }} className='text-center'>
           We have successfully registered your details.<br></br>
           We will get in touch with you shortly.
         </p>
         <div style={{
-          display:"flex",
-          justifyContent:"center"
+          display: "flex",
+          justifyContent: "center"
         }}>
           <input type="submit" onClick={() => {
             navigate("/")
-          }} name="" value="FOR MORE INFORMATION, KINDLY VISIT HERE"  className='button text-center'
-          
-          style={{
-            fontStyle:"'Garet', sans-serif;",
-            fontSize:"13px"
-          }}
+          }} name="" value="FOR MORE INFORMATION, KINDLY VISIT HERE" className='button text-center'
+
+            style={{
+              fontStyle: "'Garet', sans-serif;",
+              fontSize: "13px"
+            }}
           ></input>
         </div>
       </div>
