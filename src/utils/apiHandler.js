4eda21fc37bfc8ef2cf63d12474f7c8c5f6ebcff
Nitian Bit <155094550+nitianbit@users.noreply.github.com>
2024-10-21 12:45:08 +0530

    Update apiHandler.js

--- src/utils/apiHandler.js
+++ src/utils/apiHandler.js
@@ -18,6 +18,8 @@ const apiHandler = async (endPoint, method, data = null) => {
 
     return response.data;
   } catch (error) {
+    console.log("error api handler line 21", error)
+     console.error("error api handler line 22", error)
     if (error.response) {
       console.error("Response Error:", error.response);
       throw new Error(error.response.data.message || "Something went wrong");
